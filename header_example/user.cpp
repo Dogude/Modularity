83fcf450ecdb65b3980abe4515540b4bd25d1d4b
Dogude <131747396+Dogude@users.noreply.github.com>
2024-12-06 19:07:30 +0300

    Create user.cpp

---
+++ header_example/user.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <random>
+#include <stack>
+#include <vector>
+#include <set>
+#include <array>
+#include <list>
+#include <string>
+#include <cctype>
+#include "test.h"
+
+using namespace std;
+
+
+int main(){
+
+    Vector g(12);
+
+}
